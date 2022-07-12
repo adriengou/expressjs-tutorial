6afbfe33d5e65b8bae8f7cd78864d66f602db531
adrien gouacide <adrien.gouacide@gmail.com>
2022-07-12 16:47:56 +0200

    commit

--- modules/login.js
+++ modules/login.js
@@ -1,23 +1,74 @@
-import fh from "./modules/fileHandler.js";
 import crypto from "crypto";
 
-const path = __dirname + "/users.json";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-function register(username, password) {
-  // hash the password
+const fh = await import(`${__dirname}/fileHandler.js`);
+
+const usersPath = path.join(__dirname, "/../users.json");
+
+async function getUsers() {
+  let r = await fh.exists(usersPath);
+
+  if (r) {
+    let data = await fh.read(usersPath);
+    return JSON.parse(data);
+  } else {
+    return {};
+  }
+}
+
+async function setUsers(users) {
+  let data = JSON.stringify(users);
+  await fh.write(usersPath, data);
+}
+
+async function register(username, password) {
+  // load users.json file
+  let users = await getUsers();
+
   // check if the user is not already registered
-  // load users.json
-  // create an object with username and hashed password
-  // add object to the users object
-  // write the users object back to users.json
-}
-function login(username, password) {
+  if (users[username]) {
+    return false;
+  }
+
   // hash the password
-  // check if the user is already registered
-  // check if the user is not already logged in
-  // load users.json
-  // compare the sent password with the stored password
-  // create a hash that will be stored as a cookie by the client
-  // send back the hash
+  let hashedPassword = password; // <-- add the hashing here
+
+  // add user to the users object
+  users[username] = hashedPassword;
+
   // write the users object back to users.json
+  await setUsers(users);
+  return true;
 }
+
+async function login(username, password) {
+  // load users.json file
+  let users = await getUsers();
+
+  // check if the user is already registered
+  if (!users[username]) {
+    return false;
+  }
+
+  // hash the password
+  let hashedPassword = password; // <-- add the hashing here
+
+  // check if the user is not already logged in
+
+  // compare the sent password with the stored password
+  if (hashedPassword === users[username]) {
+    // create a hash that will be stored as a cookie by the client
+    // send back the hash
+    console.log("GOOD PASSWORD");
+    return true; //<-- return something instead
+  } else {
+    console.log("WRONG PASSWORD");
+    return false;
+  }
+}
+
+export { register, login };
